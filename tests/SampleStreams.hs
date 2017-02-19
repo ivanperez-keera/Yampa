90e199140a2512734f060d6398eb25262abc1d40
Ivan Perez <ivan.perez@keera.co.uk>
2017-02-19 22:40:26 +0000

    Documentation

--- tests/SampleStreams.hs
+++ tests/SampleStreams.hs
@@ -4,7 +4,6 @@ module SampleStreams where
 ------------------------------------------------------------------------------
 import FRP.Yampa
 import FRP.Yampa.Stream
-import FRP.Yampa.Testing
 
 -- * Streams
 
--- tests/SampleStreamsQC.hs
+++ tests/SampleStreamsQC.hs
@@ -18,46 +18,61 @@ instance Arbitrary a => Arbitrary (TimedSample a) where
     Positive dt <- arbitrary
     return (TimedSample (dt, x))
 
+-- * Stream generators
+
+-- | Streams with Uniformly spaced samples
 uniDistStream :: Arbitrary a => Gen (SignalSampleStream a)
 uniDistStream = do
   x <- arbitrary
   rest <- uniDistFutureStream
   return (x, rest)
 
+-- | Future Streams with Uniformly spaced samples
 uniDistFutureStream :: Arbitrary a => Gen (FutureSampleStream a)
 uniDistFutureStream = listOf arbitrarySample
 
-arbitrarySample :: Arbitrary a => Gen (DTime, a)
-arbitrarySample = do
-  Positive dt <- arbitrary
-  x <- arbitrary
-  return (dt, x)
-
+-- | Streams with fixed time deltas
 fixedDelayStream :: Arbitrary a => DTime -> Gen (SignalSampleStream a)
 fixedDelayStream dt = do
   x <- arbitrary
   rest <- fixedDelayFutureStream dt
   return (x, rest)
 
+-- | Future Streams with fixed time deltas
 fixedDelayFutureStream :: Arbitrary a => DTime -> Gen (FutureSampleStream a)
 fixedDelayFutureStream dt = listOf (arbitrarySampleAt dt)
 
-arbitrarySampleAt :: Arbitrary a => DTime -> Gen (DTime, a)
-arbitrarySampleAt dt = do
-  x <- arbitrary
-  return (dt, x)
-
+-- | Stream with function determining sample at a specific time
 fixedDelayStreamWith :: (DTime -> a) -> DTime -> Gen (SignalSampleStream a)
 fixedDelayStreamWith f dt = do
   rest <- fixedDelayFutureStreamWith f dt
   return (f 0.0, rest)
 
+-- | Future Stream with function determining sample at a specific time
 fixedDelayFutureStreamWith :: (DTime -> a) -> DTime -> Gen (FutureSampleStream a)
 fixedDelayFutureStreamWith f dt = listOfWith (sampleWithAt f dt)
 
+-- * Single samples
+
+-- | Arbitrary samples with random positive time deltas
+arbitrarySample :: Arbitrary a => Gen (DTime, a)
+arbitrarySample = do
+  Positive dt <- arbitrary
+  x <- arbitrary
+  return (dt, x)
+
+-- | Arbitrary samples with specific time delta
+arbitrarySampleAt :: Arbitrary a => DTime -> Gen (DTime, a)
+arbitrarySampleAt dt = do
+  x <- arbitrary
+  return (dt, x)
+
+-- | Sample at a specific time, decided by aux function
 sampleWithAt :: (DTime -> a) -> DTime -> Int -> Gen (DTime, a)
 sampleWithAt f dt i = do
   return (dt, f (fromIntegral i * dt))
+
+-- * Auxiliary functions
 
 -- | Generates a list of random length. The maximum length depends on the
 -- size parameter.
