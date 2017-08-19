4a24aa4a6f0b13732d1aed53a5f852795b724927
Ivan Perez <ivan.perez@keera.co.uk>
2017-08-19 09:22:33 +0200

    More versatile time transformation.
    [ci skip]

--- yampa/src/FRP/Yampa/Time.hs
+++ yampa/src/FRP/Yampa/Time.hs
@@ -27,6 +27,7 @@ module FRP.Yampa.Time
     ( localTime
     , time
     , timeTransform
+    , timeTransformSF
     )
   where
 
@@ -64,3 +65,20 @@ timeTransformF transform sf = SF' tf
                  in if dt' <= 0
                           then usrErr "AFRP" "timeTransform" "The time cannot be negative"
                           else (sf'', b)
+
+timeTransformSF :: SF a (DTime -> DTime)
+                -> SF a b
+                -> SF a b
+timeTransformSF transformSF sf = SF tf
+ where tf a = let (transformSFF, _) = (sfTF transformSF) a
+                  (sf', b) = (sfTF sf) a
+                  sf''     = timeTransformSFF transformSFF sf'
+              in (sf'', b)
+
+timeTransformSFF :: SF' a (DTime -> DTime) -> SF' a b -> SF' a b
+timeTransformSFF transformSF sf = SF' tf
+ where tf dt a = let (transformSF', transform) = (sfTF' transformSF) dt a
+                     dt'                           = transform dt
+                     (sf', b)                      = (sfTF' sf) dt' a
+                     sf''                          = timeTransformSFF transformSF' sf'
+                 in (sf'', b)
