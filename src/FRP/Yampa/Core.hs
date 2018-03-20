bcac0a37afae2ea4461cf09dfe267b2f1ef2f44a
Ivan Perez <ivan.perez@keera.co.uk>
2018-03-20 05:35:29 -0400

    Documents Core module.
    Closes #65.

--- src/FRP/Yampa/Core.hs
+++ src/FRP/Yampa/Core.hs
@@ -1,3 +1,16 @@
+-- | Core of Yampa, as defined in Antony Courtney's thesis
+-- [https://www.antonycourtney.com/pubs/ac-thesis.pdf, page 61].
+--
+-- Notes:
+--
+-- - While 'time' is defined as "core", it is not a primitive in Yampa, and it
+-- is actually defined as the 'integral' of 1 over time.
+--
+-- - This does not include 'derivative'.
+--
+-- - This does not include parallel switching combinators (see
+-- 'FRP.Yampa.Switches').
+--
 module FRP.Yampa.Core
     (
     -- * Signal function
@@ -17,7 +30,7 @@ module FRP.Yampa.Core
     , Event(..)
     , switch
 
-    -- ** Time (NOTE: integral 1 over time. Not really necessary.)
+    -- ** Time
     , Time
     , time
     )
