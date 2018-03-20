2992db376383a7a95e9d88f56d75f39f4d8a80f6
Ivan Perez <ivan.perez@keera.co.uk>
2018-03-20 19:21:12 -0400

    Documents Diagnostics.
    Closes #53.

--- src/FRP/Yampa/Diagnostics.hs
+++ src/FRP/Yampa/Diagnostics.hs
@@ -8,14 +8,16 @@
 -- Stability   :  provisional
 -- Portability :  portable
 --
--- Standardized error-reporting for Yampa
+-- Standarized error-reporting for Yampa
 -----------------------------------------------------------------------------------------
 
 module FRP.Yampa.Diagnostics where
 
+-- | Reports an error due to a violation of Yampa's preconditions/requirements.
 usrErr :: String -> String -> String -> a
 usrErr mn fn msg = error (mn ++ "." ++ fn ++ ": " ++ msg)
 
+-- | Reports an error in Yampa's implementation.
 intErr :: String -> String -> String -> a
 intErr mn fn msg = error ("[internal error] " ++ mn ++ "." ++ fn ++ ": "
                           ++ msg)
