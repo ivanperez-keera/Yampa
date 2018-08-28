119ebbce0fcc31e73ad3c407214712e44a81923b
Ivan Perez <ivan.perez@keera.co.uk>
2018-08-28 06:32:36 -0400

    Adds debugging module.

---
+++ examples/testing/src/FRP/Yampa/Debug.hs
@@ -0,0 +1,19 @@
+module FRP.Yampa.Debug where
+
+import Debug.Trace
+import FRP.Yampa
+import System.IO.Unsafe
+
+-- ** Debugging
+
+traceSF :: Show a
+        => SF a a
+traceSF = arr (\x -> trace x x)
+
+traceSFWith :: (a -> String)
+            => SF a a
+traceSFWith f = arr $ (\x -> trace (f x) x)
+
+traceSFWithIO :: (a -> IO b)
+              => SF a a
+traceSFWith f = arr $ (\x -> (unsafePerformIO (f x)) x)
