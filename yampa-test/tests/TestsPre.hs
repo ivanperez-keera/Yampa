e40a61dfae39122e4c456eaa0ee5a3ea0a800c91
Ivan Perez <ivan.perez@keera.co.uk>
2022-05-14 23:50:14 -0400

    yampa-test: Adjust indentation to two spaces (KSG Haskell 1.3.0 - 4.2). Refs #215.
    Suggestion: Each time a new expression or block-like construct is opened, the
    indent should increase by two spaces. When the section ends, the indent returns
    to the previous indent level. The indent level applies to both Haskell
    expressions and comments throughout the block.

--- yampa-test/tests/TestsPre.hs
+++ yampa-test/tests/TestsPre.hs
@@ -21,15 +21,15 @@ import TestsCommon
 
 pre_t0 = testSF1 (iPre 17)
 pre_t0r =
-    [ 17.0,0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0
-    , 15.0,16.0,17.0,18.0,19.0,20.0,21.0,22.0,23.0
-    ]
+  [ 17.0,0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0
+  , 15.0,16.0,17.0,18.0,19.0,20.0,21.0,22.0,23.0
+  ]
 
 pre_t1 = testSF2 (iPre 17)
 pre_t1r =
-    [ 17.0,0.0,0.0,0.0,0.0,0.0,1.0,1.0,1.0,1.0,1.0,2.0,2.0,2.0,2.0,2.0
-    , 3.0,3.0,3.0,3.0,3.0,4.0,4.0,4.0,4.0
-    ]
+  [ 17.0,0.0,0.0,0.0,0.0,0.0,1.0,1.0,1.0,1.0,1.0,2.0,2.0,2.0,2.0,2.0
+  , 3.0,3.0,3.0,3.0,3.0,4.0,4.0,4.0,4.0
+  ]
 
 pre_t2 = testSF1 (time
                   >>> arr (\t -> sin (0.5 * t * pi + pi))
@@ -37,9 +37,9 @@ pre_t2 = testSF1 (time
                             >>> second (iPre 0.0)))
 
 pre_t2r =
-    take 25
-         (let xs = [ sin (0.5 * t * pi + pi) | t <- [0.0, 0.25 ..] ]
-          in tail (scanl max 0 xs))
+  take 25
+       (let xs = [ sin (0.5 * t * pi + pi) | t <- [0.0, 0.25 ..] ]
+        in tail (scanl max 0 xs))
 
 -- This is a (somewhat strange) way of doing a counter that
 -- stops after reaching a threshold. Note that the ingoing event
@@ -48,12 +48,12 @@ pre_t2r =
 -- output without looking at the current input at all.
 pre_t3, pre_t3r :: [Int]
 pre_t3 = take 50 (embed sf (deltaEncode 0.25 (repeat ())))
-    where
-        sf = repeatedly 1.0 ()
-             >>> (loop $
-                      arr (\(e,c) -> (e `tag` (c + 1)) `gate` (c < 10))
-                      >>> dHold 0
-                      >>> arr dup)
+  where
+    sf = repeatedly 1.0 ()
+         >>> (loop $
+                arr (\(e,c) -> (e `tag` (c + 1)) `gate` (c < 10))
+                >>> dHold 0
+                >>> arr dup)
 pre_t3r = [ 0,0,0,0      -- 0s
           , 0,1,1,1      -- 1s
           , 1,2,2,2      -- 2s
@@ -73,12 +73,12 @@ pre_t3r = [ 0,0,0,0      -- 0s
 -- there is an initial event.
 pre_t4, pre_t4r :: [Int]
 pre_t4 = take 50 (embed sf (deltaEncode 0.25 (repeat ())))
-    where
-        sf = (now () &&& repeatedly 1.0 ()) >>> arr (uncurry lMerge)
-             >>> (loop $
-                      arr (\(e,c) -> (e `tag` (c + 1)) `gate` (c < 10))
-                      >>> dHold 0
-                      >>> arr dup)
+  where
+    sf = (now () &&& repeatedly 1.0 ()) >>> arr (uncurry lMerge)
+         >>> (loop $
+                arr (\(e,c) -> (e `tag` (c + 1)) `gate` (c < 10))
+                >>> dHold 0
+                >>> arr dup)
 pre_t4r = [ 0,1,1,1      -- 0s
           , 1,2,2,2      -- 1s
           , 2,3,3,3      -- 2s
@@ -97,12 +97,12 @@ pre_t4r = [ 0,1,1,1      -- 0s
 -- Similar test to "pre_t3" above but for dAccumHold.
 pre_t5, pre_t5r :: [Int]
 pre_t5 = take 50 (embed sf (deltaEncode 0.25 (repeat ())))
-    where
-        sf = repeatedly 1.0 ()
-             >>> (loop $
-                      arr (\(e,c) -> (e `tag` (+1)) `gate` (c < 10))
-                      >>> dAccumHold 0
-                      >>> arr dup)
+  where
+    sf = repeatedly 1.0 ()
+         >>> (loop $
+                arr (\(e,c) -> (e `tag` (+1)) `gate` (c < 10))
+                >>> dAccumHold 0
+                >>> arr dup)
 pre_t5r = [ 0,0,0,0      -- 0s
           , 0,1,1,1      -- 1s
           , 1,2,2,2      -- 2s
@@ -121,12 +121,12 @@ pre_t5r = [ 0,0,0,0      -- 0s
 -- Similar test to "pre_t4" above but for dAccumHold.
 pre_t6, pre_t6r :: [Int]
 pre_t6 = take 50 (embed sf (deltaEncode 0.25 (repeat ())))
-    where
-        sf = (now () &&& repeatedly 1.0 ()) >>> arr (uncurry lMerge)
-             >>> (loop $
-                      arr (\(e,c) -> (e `tag` (+1)) `gate` (c < 10))
-                      >>> dAccumHold 0
-                      >>> arr dup)
+  where
+    sf = (now () &&& repeatedly 1.0 ()) >>> arr (uncurry lMerge)
+         >>> (loop $
+                arr (\(e,c) -> (e `tag` (+1)) `gate` (c < 10))
+                >>> dAccumHold 0
+                >>> arr dup)
 pre_t6r = [ 0,1,1,1      -- 0s
           , 1,2,2,2      -- 1s
           , 2,3,3,3      -- 2s
@@ -145,12 +145,12 @@ pre_t6r = [ 0,1,1,1      -- 0s
 -- Similar test to "pre_t3" above but for dAccumHoldBy.
 pre_t7, pre_t7r :: [Int]
 pre_t7 = take 50 (embed sf (deltaEncode 0.25 (repeat ())))
-    where
-        sf = repeatedly 1.0 ()
-             >>> (loop $
-                      arr (\(e,c) -> e `gate` (c < 10))
-                      >>> dAccumHoldBy (\c _ -> c + 1) 0
-                      >>> arr dup)
+  where
+    sf = repeatedly 1.0 ()
+         >>> (loop $
+                arr (\(e,c) -> e `gate` (c < 10))
+                >>> dAccumHoldBy (\c _ -> c + 1) 0
+                >>> arr dup)
 pre_t7r = [ 0,0,0,0      -- 0s
           , 0,1,1,1      -- 1s
           , 1,2,2,2      -- 2s
@@ -169,12 +169,12 @@ pre_t7r = [ 0,0,0,0      -- 0s
 -- Similar test to "pre_t4" above but for dAccumHoldBy.
 pre_t8, pre_t8r :: [Int]
 pre_t8 = take 50 (embed sf (deltaEncode 0.25 (repeat ())))
-    where
-        sf = (now () &&& repeatedly 1.0 ()) >>> arr (uncurry lMerge)
-             >>> (loop $
-                      arr (\(e,c) -> e `gate` (c < 10))
-                      >>> dAccumHoldBy (\c _ -> c + 1) 0
-                      >>> arr dup)
+  where
+    sf = (now () &&& repeatedly 1.0 ()) >>> arr (uncurry lMerge)
+         >>> (loop $
+                arr (\(e,c) -> e `gate` (c < 10))
+                >>> dAccumHoldBy (\c _ -> c + 1) 0
+                >>> arr dup)
 pre_t8r = [ 0,1,1,1      -- 0s
           , 1,2,2,2      -- 1s
           , 2,3,3,3      -- 2s
@@ -191,15 +191,15 @@ pre_t8r = [ 0,1,1,1      -- 0s
           ]
 
 pre_trs =
-    [ pre_t0 ~= pre_t0r
-    , pre_t1 ~= pre_t1r
-    , pre_t2 ~= pre_t2r
-    , pre_t3 == pre_t3r
-    , pre_t4 == pre_t4r
-    , pre_t5 == pre_t5r
-    , pre_t6 == pre_t6r
-    , pre_t7 == pre_t7r
-    , pre_t8 == pre_t8r
-    ]
+  [ pre_t0 ~= pre_t0r
+  , pre_t1 ~= pre_t1r
+  , pre_t2 ~= pre_t2r
+  , pre_t3 == pre_t3r
+  , pre_t4 == pre_t4r
+  , pre_t5 == pre_t5r
+  , pre_t6 == pre_t6r
+  , pre_t7 == pre_t7r
+  , pre_t8 == pre_t8r
+  ]
 
 pre_tr = and pre_trs
