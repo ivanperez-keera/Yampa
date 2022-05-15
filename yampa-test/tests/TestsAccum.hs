7b1063dffeee8dcd561bdf3cf7c67759d20c386b
Ivan Perez <ivan.perez@keera.co.uk>
2022-05-14 21:40:44 -0400

    yampa-test: Align by comma (KSG Haskell 1.3.0 - 4.2). Refs #213.
    For lists or tuple elements separated by commas in which each value is listed
    in a separate line, and when the Haskell syntax allows it, the comma must be
    the ﬁrst character of every line but the first.
    
    This commit adjust commas and indentation of records, lists or tuples, so that
    the comma leads the line instead of following it.

--- yampa-test/tests/TestsAccum.hs
+++ yampa-test/tests/TestsAccum.hs
@@ -25,33 +25,37 @@ import TestsCommon
 accum_inp1 = (fromJust (head delta_inp), zip (repeat 1.0) (tail delta_inp))
     where
         delta_inp =
-            [Just NoEvent, Nothing, Just (Event (+1.0)), Just NoEvent,
-             Just (Event (+2.0)), Just NoEvent, Nothing, Nothing,
-             Just (Event (*3.0)), Just (Event (+5.0)), Nothing, Just NoEvent,
-             Just (Event (/2.0)), Just NoEvent, Nothing, Nothing]
+            [ Just NoEvent, Nothing, Just (Event (+1.0)), Just NoEvent
+            , Just (Event (+2.0)), Just NoEvent, Nothing, Nothing
+            , Just (Event (*3.0)), Just (Event (+5.0)), Nothing, Just NoEvent
+            , Just (Event (/2.0)), Just NoEvent, Nothing, Nothing
+            ]
             ++ repeat Nothing
 
 accum_inp2 = (fromJust (head delta_inp), zip (repeat 1.0) (tail delta_inp))
     where
         delta_inp =
-            [Just (Event (+1.0)), Just NoEvent, Nothing, Nothing,
-             Just (Event (+2.0)), Just NoEvent, Nothing, Nothing,
-             Just (Event (*3.0)), Just (Event (+5.0)), Nothing, Just NoEvent,
-             Just (Event (/2.0)), Just NoEvent, Nothing, Nothing]
+            [ Just (Event (+1.0)), Just NoEvent, Nothing, Nothing
+            , Just (Event (+2.0)), Just NoEvent, Nothing, Nothing
+            , Just (Event (*3.0)), Just (Event (+5.0)), Nothing, Just NoEvent
+            , Just (Event (/2.0)), Just NoEvent, Nothing, Nothing
+            ]
             ++ repeat Nothing
 
 accum_inp3 = deltaEncode 1.0 $
-    [NoEvent,   NoEvent,   Event 1.0, NoEvent,
-     Event 2.0, NoEvent,   NoEvent,   NoEvent,
-     Event 3.0, Event 5.0, Event 5.0, NoEvent,
-     Event 0.0, NoEvent,   NoEvent,   NoEvent]
+    [ NoEvent,   NoEvent,   Event 1.0, NoEvent
+    , Event 2.0, NoEvent,   NoEvent,   NoEvent
+    , Event 3.0, Event 5.0, Event 5.0, NoEvent
+    , Event 0.0, NoEvent,   NoEvent,   NoEvent
+    ]
     ++ repeat NoEvent
 
 accum_inp4 = deltaEncode 1.0 $
-    [Event 1.0, NoEvent,   NoEvent,   NoEvent,
-     Event 2.0, NoEvent,   NoEvent,   NoEvent,
-     Event 3.0, Event 5.0, Event 5.0, NoEvent,
-     Event 0.0, NoEvent,   NoEvent,   NoEvent]
+    [ Event 1.0, NoEvent,   NoEvent,   NoEvent
+    , Event 2.0, NoEvent,   NoEvent,   NoEvent
+    , Event 3.0, Event 5.0, Event 5.0, NoEvent
+    , Event 0.0, NoEvent,   NoEvent,   NoEvent
+    ]
     ++ repeat NoEvent
 
 
@@ -62,20 +66,22 @@ accum_t0 :: [Event Double]
 accum_t0 = take 16 $ embed (accum 0.0) accum_inp1
 
 accum_t0r =
-    [NoEvent,   NoEvent,    Event 1.0,  NoEvent,
-     Event 3.0, NoEvent,    NoEvent,    NoEvent,
-     Event 9.0, Event 14.0, Event 19.0, NoEvent,
-     Event 9.5, NoEvent,    NoEvent,    NoEvent]
+    [ NoEvent,   NoEvent,    Event 1.0,  NoEvent
+    , Event 3.0, NoEvent,    NoEvent,    NoEvent
+    , Event 9.0, Event 14.0, Event 19.0, NoEvent
+    , Event 9.5, NoEvent,    NoEvent,    NoEvent
+    ]
 
 
 accum_t1 :: [Event Double]
 accum_t1 = take 16 $ embed (accum 0.0) accum_inp2
 
 accum_t1r =
-    [Event 1.0, NoEvent,    NoEvent,    NoEvent,
-     Event 3.0, NoEvent,    NoEvent,    NoEvent,
-     Event 9.0, Event 14.0, Event 19.0, NoEvent,
-     Event 9.5, NoEvent,    NoEvent,    NoEvent]
+    [ Event 1.0, NoEvent,    NoEvent,    NoEvent
+    , Event 3.0, NoEvent,    NoEvent,    NoEvent
+    , Event 9.0, Event 14.0, Event 19.0, NoEvent
+    , Event 9.5, NoEvent,    NoEvent,    NoEvent
+    ]
 
 
 accum_t2 :: [Event Int]
@@ -83,10 +89,11 @@ accum_t2 = take 16 $ embed (accumBy (\a d -> a + floor d) 0) accum_inp3
 
 accum_t2r :: [Event Int]
 accum_t2r =
-    [NoEvent,  NoEvent,  Event 1,  NoEvent,
-     Event 3,  NoEvent,  NoEvent,  NoEvent,
-     Event 6,  Event 11, Event 16, NoEvent,
-     Event 16, NoEvent,  NoEvent,  NoEvent]
+    [ NoEvent,  NoEvent,  Event 1,  NoEvent
+    , Event 3,  NoEvent,  NoEvent,  NoEvent
+    , Event 6,  Event 11, Event 16, NoEvent
+    , Event 16, NoEvent,  NoEvent,  NoEvent
+    ]
 
 
 accum_t3 :: [Event Int]
@@ -94,10 +101,11 @@ accum_t3 = take 16 $ embed (accumBy (\a d -> a + floor d) 0) accum_inp4
 
 accum_t3r :: [Event Int]
 accum_t3r =
-    [Event 1,  NoEvent,  NoEvent,  NoEvent,
-     Event 3,  NoEvent,  NoEvent,  NoEvent,
-     Event 6,  Event 11, Event 16, NoEvent,
-     Event 16, NoEvent,  NoEvent,  NoEvent]
+    [ Event 1,  NoEvent,  NoEvent,  NoEvent
+    , Event 3,  NoEvent,  NoEvent,  NoEvent
+    , Event 6,  Event 11, Event 16, NoEvent
+    , Event 16, NoEvent,  NoEvent,  NoEvent
+    ]
 
 
 accum_accFiltFun1 a d =
@@ -113,10 +121,11 @@ accum_t4 = take 16 $ embed (accumFilter accum_accFiltFun1 0) accum_inp3
 
 accum_t4r :: [Event (Bool,Int)]
 accum_t4r =
-    [NoEvent,         NoEvent, NoEvent,         NoEvent,
-     NoEvent,         NoEvent, NoEvent,         NoEvent,
-     Event (False,6), NoEvent, Event (True,16), NoEvent,
-     Event (True,16), NoEvent, NoEvent,         NoEvent]
+    [ NoEvent,         NoEvent, NoEvent,         NoEvent
+    , NoEvent,         NoEvent, NoEvent,         NoEvent
+    , Event (False,6), NoEvent, Event (True,16), NoEvent
+    , Event (True,16), NoEvent, NoEvent,         NoEvent
+    ]
 
 
 accum_accFiltFun2 a d =
@@ -132,10 +141,11 @@ accum_t5 = take 16 $ embed (accumFilter accum_accFiltFun2 0) accum_inp4
 
 accum_t5r :: [Event (Bool,Int)]
 accum_t5r =
-    [Event (False,1), NoEvent,         NoEvent, NoEvent,
-     Event (False,3), NoEvent,         NoEvent, NoEvent,
-     NoEvent,         Event (True,11), NoEvent, NoEvent,
-     NoEvent,         NoEvent,         NoEvent, NoEvent]
+    [ Event (False,1), NoEvent,         NoEvent, NoEvent
+    , Event (False,3), NoEvent,         NoEvent, NoEvent
+    , NoEvent,         Event (True,11), NoEvent, NoEvent
+    , NoEvent,         NoEvent,         NoEvent, NoEvent
+    ]
 
 
 -- This can be seen as the definition of accumFilter
@@ -169,16 +179,17 @@ accum_t8 = take 40 $ embed (repeatedly 1.0 1
                            accum_inp5
 
 accum_t8r :: [Event Int]
-accum_t8r = [NoEvent,  NoEvent, NoEvent, NoEvent,
-             Event 1,  NoEvent, NoEvent, NoEvent,
-             Event 3,  NoEvent, NoEvent, NoEvent,
-             Event 6,  NoEvent, NoEvent, NoEvent,
-             Event 10, NoEvent, NoEvent, NoEvent,
-             Event 15, NoEvent, NoEvent, NoEvent,
-             Event 21, NoEvent, NoEvent, NoEvent,
-             Event 28, NoEvent, NoEvent, NoEvent,
-             Event 36, NoEvent, NoEvent, NoEvent,
-             Event 45, NoEvent, NoEvent, NoEvent]
+accum_t8r = [ NoEvent,  NoEvent, NoEvent, NoEvent
+            , Event 1,  NoEvent, NoEvent, NoEvent
+            , Event 3,  NoEvent, NoEvent, NoEvent
+            , Event 6,  NoEvent, NoEvent, NoEvent
+            , Event 10, NoEvent, NoEvent, NoEvent
+            , Event 15, NoEvent, NoEvent, NoEvent
+            , Event 21, NoEvent, NoEvent, NoEvent
+            , Event 28, NoEvent, NoEvent, NoEvent
+            , Event 36, NoEvent, NoEvent, NoEvent
+            , Event 45, NoEvent, NoEvent, NoEvent
+            ]
 
 
 accum_t9 :: [Int]
@@ -189,8 +200,9 @@ accum_t9 = take 40 $ embed (repeatedly 1.0 1
                            accum_inp5
 
 accum_t9r :: [Int]
-accum_t9r = [0,0,0,0,1,1,1,1,3,3,3,3,6,6,6,6,10,10,10,10,15,15,15,15,
-             21,21,21,21,28,28,28,28,36,36,36,36,45,45,45,45]
+accum_t9r = [ 0,0,0,0,1,1,1,1,3,3,3,3,6,6,6,6,10,10,10,10,15,15,15,15
+            , 21,21,21,21,28,28,28,28,36,36,36,36,45,45,45,45
+            ]
 
 
 accum_t10 :: [Int]
@@ -211,8 +223,9 @@ accum_t11 = take 40 $ embed (repeatedly 1.0 1
                             accum_inp5
 
 accum_t11r :: [Int]
-accum_t11r = [0,0,0,0,0,1,1,1,1,3,3,3,3,6,6,6,6,10,10,10,10,15,15,15,
-              15,21,21,21,21,28,28,28,28,36,36,36,36,45,45,45]
+accum_t11r = [ 0,0,0,0,0,1,1,1,1,3,3,3,3,6,6,6,6,10,10,10,10,15,15,15
+             , 15,21,21,21,21,28,28,28,28,36,36,36,36,45,45,45
+             ]
 
 
 accum_t12 :: [Int]
@@ -243,16 +256,17 @@ accum_t13 = take 40 $ embed (repeatedly 1.0 1
                             accum_inp5
 
 accum_t13r :: [Event Int]
-accum_t13r = [NoEvent,  NoEvent, NoEvent, NoEvent,
-              Event 1,  NoEvent, NoEvent, NoEvent,
-              NoEvent,  NoEvent, NoEvent, NoEvent,
-              Event 5,  NoEvent, NoEvent, NoEvent,
-              NoEvent,  NoEvent, NoEvent, NoEvent,
-              Event 14, NoEvent, NoEvent, NoEvent,
-              NoEvent,  NoEvent, NoEvent, NoEvent,
-              Event 30, NoEvent, NoEvent, NoEvent,
-              NoEvent,  NoEvent, NoEvent, NoEvent,
-              Event 55, NoEvent, NoEvent, NoEvent]
+accum_t13r = [ NoEvent,  NoEvent, NoEvent, NoEvent
+             , Event 1,  NoEvent, NoEvent, NoEvent
+             , NoEvent,  NoEvent, NoEvent, NoEvent
+             , Event 5,  NoEvent, NoEvent, NoEvent
+             , NoEvent,  NoEvent, NoEvent, NoEvent
+             , Event 14, NoEvent, NoEvent, NoEvent
+             , NoEvent,  NoEvent, NoEvent, NoEvent
+             , Event 30, NoEvent, NoEvent, NoEvent
+             , NoEvent,  NoEvent, NoEvent, NoEvent
+             , Event 55, NoEvent, NoEvent, NoEvent
+             ]
 
 
 accum_t14 :: [Int]
@@ -264,8 +278,9 @@ accum_t14 = take 40 $ embed (repeatedly 1.0 1
                             accum_inp5
 
 accum_t14r :: [Int]
-accum_t14r = [0,0,0,0,1,1,1,1,1,1,1,1,5,5,5,5,5,5,5,5,14,14,14,14,
-              14,14,14,14,30,30,30,30,30,30,30,30,55,55,55,55]
+accum_t14r = [ 0,0,0,0,1,1,1,1,1,1,1,1,5,5,5,5,5,5,5,5,14,14,14,14
+             , 14,14,14,14,30,30,30,30,30,30,30,30,55,55,55,55
+             ]
 
 
 accum_t15 :: [Int]
@@ -288,8 +303,9 @@ accum_t16 = take 40 $ embed (repeatedly 1.0 1
                             accum_inp5
 
 accum_t16r :: [Int]
-accum_t16r = [0,0,0,0,0,1,1,1,1,1,1,1,1,5,5,5,5,5,5,5,5,14,14,14,
-              14,14,14,14,14,30,30,30,30,30,30,30,30,55,55,55]
+accum_t16r = [ 0,0,0,0,0,1,1,1,1,1,1,1,1,5,5,5,5,5,5,5,5,14,14,14
+             , 14,14,14,14,14,30,30,30,30,30,30,30,30,55,55,55
+             ]
 
 
 accum_t17 :: [Int]
@@ -305,24 +321,24 @@ accum_t17r = accum_t16 -- Should agree!
 
 
 accum_trs =
-    [ accum_t0  == accum_t0r,
-      accum_t1  == accum_t1r,
-      accum_t2  == accum_t2r,
-      accum_t3  == accum_t3r,
-      accum_t4  == accum_t4r,
-      accum_t5  == accum_t5r,
-      accum_t6  == accum_t6r,
-      accum_t7  == accum_t7r,
-      accum_t8  == accum_t8r,
-      accum_t9  == accum_t9r,
-      accum_t10 == accum_t10r,
-      accum_t11 == accum_t11r,
-      accum_t12 == accum_t12r,
-      accum_t13 == accum_t13r,
-      accum_t14 == accum_t14r,
-      accum_t15 == accum_t15r,
-      accum_t16 == accum_t16r,
-      accum_t17 == accum_t17r
+    [ accum_t0  == accum_t0r
+    , accum_t1  == accum_t1r
+    , accum_t2  == accum_t2r
+    , accum_t3  == accum_t3r
+    , accum_t4  == accum_t4r
+    , accum_t5  == accum_t5r
+    , accum_t6  == accum_t6r
+    , accum_t7  == accum_t7r
+    , accum_t8  == accum_t8r
+    , accum_t9  == accum_t9r
+    , accum_t10 == accum_t10r
+    , accum_t11 == accum_t11r
+    , accum_t12 == accum_t12r
+    , accum_t13 == accum_t13r
+    , accum_t14 == accum_t14r
+    , accum_t15 == accum_t15r
+    , accum_t16 == accum_t16r
+    , accum_t17 == accum_t17r
     ]
 
 accum_tr = and accum_trs
