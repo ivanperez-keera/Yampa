e8af44c6835e1a7443e258fcbf8c5a0b893a224e
Ivan Perez <ivan.perez@keera.co.uk>
2022-05-21 21:05:34 -0400

    yampa-test: Organize module EventS following Yampa's order. Refs #217.
    Understanding which parts of Yampa are tested and which ones are not is
    generally hard. To facilitate keeping track of which elements are tested
    and which ones are not, this commit organizes the elements of EventS in
    the same order and with the same haddock section separators as they have
    in Yampa's counterpart.

--- yampa-test/tests/Test/FRP/Yampa/EventS.hs
+++ yampa-test/tests/Test/FRP/Yampa/EventS.hs
@@ -30,9 +30,12 @@ import TestsCommon
 
 tests :: TestTree
 tests = testGroup "Regression tests for FRP.Yampa.EventS"
-  [ testProperty "eventS (0, fixed)"       (property $ evsrc_t0 ~= evsrc_t0r)
+  [ testProperty "never (0, fixed)"        (property $ evsrc_t0 ~= evsrc_t0r)
+  , testProperty "Events > No event"       prop_event_noevent
   , testProperty "eventS (1, fixed)"       (property $ evsrc_t1 ~= evsrc_t1r)
+  , testProperty "Events > Now"            prop_event_now
   , testProperty "eventS (2, fixed)"       (property $ evsrc_t2 ~= evsrc_t2r)
+  , testProperty "Events > After 0.0"      prop_event_after_0
   , testProperty "eventS (3, fixed)"       (property $ evsrc_t3 ~= evsrc_t3r)
   , testProperty "eventS (4, fixed)"       (property $ evsrc_t4 ~= evsrc_t4r)
   , testProperty "eventS (5, fixed)"       (property $ evsrc_t5 ~= evsrc_t5r)
@@ -42,6 +45,9 @@ tests = testGroup "Regression tests for FRP.Yampa.EventS"
   , testProperty "eventS (9, fixed)"       (property $ evsrc_t9 ~= evsrc_t9r)
   , testProperty "eventS (10, fixed)"      (property $ evsrc_t10 ~= evsrc_t10r)
   , testProperty "eventS (11, fixed)"      (property $ evsrc_t11 ~= evsrc_t11r)
+  , testProperty "eventS (28, fixed)"      (property $ evsrc_t28 ~= evsrc_t28r)
+  , testProperty "eventS (30, fixed)"      (property $ evsrc_t30 ~= evsrc_t30r)
+  , testProperty "eventS (29, fixed)"      (property $ evsrc_t29 ~= evsrc_t29r)
   , testProperty "eventS (12, fixed)"      (property $ evsrc_t12 ~= evsrc_t12r)
   , testProperty "eventS (13, fixed)"      (property $ evsrc_t13 ~= evsrc_t13r)
   , testProperty "eventS (14, fixed)"      (property $ evsrc_t14 ~= evsrc_t14r)
@@ -58,22 +64,16 @@ tests = testGroup "Regression tests for FRP.Yampa.EventS"
   , testProperty "eventS (25, fixed)"      (property $ evsrc_t25 ~= evsrc_t25r)
   , testProperty "eventS (26, fixed)"      (property $ evsrc_t26 ~= evsrc_t26r)
   , testProperty "eventS (27, fixed)"      (property $ evsrc_t27 ~= evsrc_t27r)
-  , testProperty "eventS (28, fixed)"      (property $ evsrc_t28 ~= evsrc_t28r)
-  , testProperty "eventS (29, fixed)"      (property $ evsrc_t29 ~= evsrc_t29r)
-  , testProperty "eventS (30, fixed)"      (property $ evsrc_t30 ~= evsrc_t30r)
   , testProperty "snap (fixed)"            (property $ utils_t10 ~= utils_t10r)
   , testProperty "snapAfter (fixed)"       (property $ utils_t11 ~= utils_t11r)
   , testProperty "sample (fixed)"          (property $ utils_t12 ~= utils_t12r)
-  , testProperty "after (0, fixed)"        (property $ utils_t13 ~= utils_t13r)
-  , testProperty "after (1, fixed)"        (property $ utils_t14 ~= utils_t14r)
   , testProperty "sampleWindow (0, fixed)" (property $ utils_t15 ~= utils_t15r)
   , testProperty "sampleWindow (1, fixed)" (property $ utils_t16 ~= utils_t16r)
-  , testProperty "Events > No event"       prop_event_noevent
-  , testProperty "Events > Now"            prop_event_now
-  , testProperty "Events > After 0.0"      prop_event_after_0
+  , testProperty "after (0, fixed)"        (property $ utils_t13 ~= utils_t13r)
+  , testProperty "after (1, fixed)"        (property $ utils_t14 ~= utils_t14r)
   ]
 
--- * Test cases for basic event sources and stateful event suppression
+-- * Basic event sources
 
 evsrc_t0 :: [Event ()]
 evsrc_t0 = testSF1 never
@@ -87,6 +87,14 @@ evsrc_t0r =
   , NoEvent, NoEvent, NoEvent, NoEvent  -- 5.0 s
   , NoEvent
   ]
+
+prop_event_noevent =
+    forAll myStream $ evalT $ Always $ prop (sfNever, const (== noEvent))
+
+  where myStream :: Gen (SignalSampleStream Float)
+        myStream = uniDistStream
+        sfNever :: SF Float (Event Float)
+        sfNever = never
 
 evsrc_t1 :: [Event Int]
 evsrc_t1 = testSF1 (now 42)
@@ -102,6 +110,20 @@ evsrc_t1r =
   , NoEvent
   ]
 
+prop_event_now =
+    forAll myStream $ evalT $
+      -- (sf, p0) /\ O [] (sf, pn)
+      And (prop (sf, p0))                 -- Initially
+          (Next $ Always $ prop (sf, pn)) -- After first sample
+
+  where sf = Yampa.now 42.0
+
+        p0 x y = y == Event 42.0
+        pn x y = y == noEvent
+
+        myStream :: Gen (SignalSampleStream Float)
+        myStream = uniDistStream
+
 evsrc_t2 :: [Event Int]
 evsrc_t2 = testSF1 (after 0.0 42)
 evsrc_t2r :: [Event Int]
@@ -114,6 +136,20 @@ evsrc_t2r =
   , NoEvent,  NoEvent, NoEvent, NoEvent  -- 5.0 s
   , NoEvent
   ]
+
+prop_event_after_0 =
+    forAll myStream $ evalT $
+      -- (sf, p0) /\ O [] (sf, pn)
+      And (prop (sf, p0))                 -- Initially
+          (Next $ Always $ prop (sf, pn)) -- After first sample
+
+  where sf = after 0.0 42.0
+
+        p0 x y = y == Event 42.0
+        pn x y = y == noEvent
+
+        myStream :: Gen (SignalSampleStream Float)
+        myStream = uniDistStream
 
 evsrc_t3 :: [Event Int]
 evsrc_t3 = testSF1 (after 3.0 42)
@@ -259,6 +295,119 @@ evsrc_t11r =
   , NoEvent
   ]
 
+evsrc_t28 :: [(Event Int, Event Int)]
+evsrc_t28 = embed (repeatedly 0.5 ()
+                   >>> accumBy (\n _ -> n + 1) 0
+                   >>> identity &&& delayEvent 2.0)
+                  (deltaEncode 0.125 (replicate 50 ()))
+
+evsrc_t28r =
+  [ (NoEvent,NoEvent),  (NoEvent,NoEvent)  -- 0.0 s
+  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
+  , (Event 1,NoEvent),  (NoEvent,NoEvent)  -- 0.5 s
+  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
+  , (Event 2,NoEvent),  (NoEvent,NoEvent)  -- 1.0 s
+  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
+  , (Event 3,NoEvent),  (NoEvent,NoEvent)  -- 1.5 s
+  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
+  , (Event 4,NoEvent),  (NoEvent,NoEvent)  -- 2.0 s
+  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
+  , (Event 5,Event 1),  (NoEvent,NoEvent)  -- 2.5 s
+  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
+  , (Event 6,Event 2),  (NoEvent,NoEvent)  -- 3.0 s
+  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
+  , (Event 7,Event 3),  (NoEvent,NoEvent)  -- 3.5 s
+  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
+  , (Event 8,Event 4),  (NoEvent,NoEvent)  -- 4.0 s
+  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
+  , (Event 9,Event 5),  (NoEvent,NoEvent)  -- 4.5 s
+  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
+  , (Event 10,Event 6), (NoEvent,NoEvent)  -- 5.0 s
+  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
+  , (Event 11,Event 7), (NoEvent,NoEvent)  -- 5.5 s
+  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
+  , (Event 12,Event 8), (NoEvent,NoEvent)  -- 6.0 s
+  ]
+
+-- "delayEvent" in a feedback loop. Should work like "repeatedly".
+evsrc_t30 :: [(Event ())]
+evsrc_t30 = embed (now ()
+                   >>> (loop $
+                          arr (uncurry lMerge)
+                          >>> delayEvent 1.0
+                          >>> arr dup))
+                  (deltaEncode 0.125 (replicate 50 ()))
+
+evsrc_t30r :: [(Event ())]
+evsrc_t30r =
+  [ NoEvent,  NoEvent, NoEvent, NoEvent  -- 0.0 s
+  , NoEvent,  NoEvent, NoEvent, NoEvent  -- 0.5 s
+  , Event (), NoEvent, NoEvent, NoEvent  -- 1.0 s
+  , NoEvent,  NoEvent, NoEvent, NoEvent  -- 1.5 s
+  , Event (), NoEvent, NoEvent, NoEvent  -- 2.0 s
+  , NoEvent,  NoEvent, NoEvent, NoEvent  -- 2.5 s
+  , Event (), NoEvent, NoEvent, NoEvent  -- 3.0 s
+  , NoEvent,  NoEvent, NoEvent, NoEvent  -- 3.5 s
+  , Event (), NoEvent, NoEvent, NoEvent  -- 4.0 s
+  , NoEvent,  NoEvent, NoEvent, NoEvent  -- 4.5 s
+  , Event (), NoEvent, NoEvent, NoEvent  -- 5.0 s
+  , NoEvent,  NoEvent, NoEvent, NoEvent  -- 5.5 s
+  , Event (), NoEvent                    -- 6.0 s
+  ]
+
+evsrc_t29 :: [Event [Double]]
+evsrc_t29 = embed (time &&& repeatedly 0.5001 ()
+                   >>> arr (\(t,e) -> e `tag` t)
+                   >>> delayEventCat 3.0)
+                  input
+  where
+    dts   = replicate 40 0.1 ++ [2.0] ++ replicate 40 0.1
+    input = ((), [(dt, Just ()) | dt <- dts])
+
+{- Resulting input to the delay for reference:
+[ NoEvent,   NoEvent,   NoEvent, NoEvent, NoEvent  -- 0.0 s
+, NoEvent,   Event 0.6, NoEvent, NoEvent, NoEvent  -- 0.5 s
+, NoEvent,   Event 1.1, NoEvent, NoEvent, NoEvent  -- 1.0 s
+, NoEvent,   Event 1.6, NoEvent, NoEvent, NoEvent  -- 1.5 s
+, NoEvent,   Event 2.1, NoEvent, NoEvent, NoEvent  -- 2.0 s
+, NoEvent,   Event 2.6, NoEvent, NoEvent, NoEvent  -- 2.5 s
+, NoEvent,   Event 3.1, NoEvent, NoEvent, NoEvent  -- 3.0 s
+, NoEvent,   Event 3.6, NoEvent, NoEvent, NoEvent  -- 3.5 s
+, NoEvent                                          -- 4.0 s
+, Event 6.0, Event 6.1, NoEvent, NoEvent, NoEvent  -- 6.0 s
+, NoEvent,   Event 6.6, NoEvent, NoEvent, NoEvent  -- 6.5 s
+, NoEvent,   Event 7.1, NoEvent, NoEvent, NoEvent  -- 7.0 s
+, NoEvent,   Event 7.6, NoEvent, NoEvent, NoEvent  -- 7.5 s
+, NoEvent,   Event 8.1, NoEvent, NoEvent, NoEvent  -- 8.0 s
+, NoEvent,   Event 8.6, NoEvent, NoEvent, NoEvent  -- 8.5 s
+, NoEvent,   Event 9.1, NoEvent, NoEvent, NoEvent  -- 9.0 s
+, NoEvent,   Event 9.6, NoEvent, NoEvent, NoEvent  -- 9.5 s
+, NoEvent                                          -- 10.0 s
+]
+-}
+
+evsrc_t29r =
+  [ NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 0.0 s
+  , NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 0.5 s
+  , NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 1.0 s
+  , NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 1.5 s
+  , NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 2.0 s
+  , NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 2.5 s
+  , NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 3.0 s
+  , NoEvent, Event [0.6], NoEvent, NoEvent, NoEvent  -- 3.5 s
+  , NoEvent                                          -- 4.0 s
+  , Event [1.1, 1.6, 2.1, 2.6]                       -- 6.0 s
+           , NoEvent,     Event [3.1], NoEvent, NoEvent
+  , NoEvent, NoEvent,     Event [3.6], NoEvent, NoEvent  -- 6.5 s
+  , NoEvent, NoEvent,     NoEvent,     NoEvent, NoEvent  -- 7.0 s
+  , NoEvent, NoEvent,     NoEvent,     NoEvent, NoEvent  -- 7.5 s
+  , NoEvent, NoEvent,     NoEvent,     NoEvent, NoEvent  -- 8.0 s
+  , NoEvent, NoEvent,     NoEvent,     NoEvent, NoEvent  -- 8.5 s
+  , NoEvent, Event [6.0], Event [6.1], NoEvent, NoEvent  -- 9.0 s
+  , NoEvent, NoEvent,     Event [6.6], NoEvent, NoEvent  -- 9.5 s
+  , NoEvent                                              -- 10.0 s
+  ]
+
 evsrc_t12 :: [Event ()]
 evsrc_t12 = testSF1 (localTime >>> arr (>=0) >>> edge)
 
@@ -337,6 +486,8 @@ evsrc_t16r =
   , NoEvent,     NoEvent,    NoEvent, NoEvent      -- 5.0 s
   , NoEvent
   ]
+
+-- * Stateful event suppression
 
 evsrc_t17 :: [Event Int]
 evsrc_t17 = testSF1 (now 17 &&& repeatedly 0.795 42
@@ -496,120 +647,7 @@ evsrc_t27r =
   , Event 42
   ]
 
-evsrc_t28 :: [(Event Int, Event Int)]
-evsrc_t28 = embed (repeatedly 0.5 ()
-                   >>> accumBy (\n _ -> n + 1) 0
-                   >>> identity &&& delayEvent 2.0)
-                  (deltaEncode 0.125 (replicate 50 ()))
-
-evsrc_t28r =
-  [ (NoEvent,NoEvent),  (NoEvent,NoEvent)  -- 0.0 s
-  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
-  , (Event 1,NoEvent),  (NoEvent,NoEvent)  -- 0.5 s
-  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
-  , (Event 2,NoEvent),  (NoEvent,NoEvent)  -- 1.0 s
-  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
-  , (Event 3,NoEvent),  (NoEvent,NoEvent)  -- 1.5 s
-  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
-  , (Event 4,NoEvent),  (NoEvent,NoEvent)  -- 2.0 s
-  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
-  , (Event 5,Event 1),  (NoEvent,NoEvent)  -- 2.5 s
-  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
-  , (Event 6,Event 2),  (NoEvent,NoEvent)  -- 3.0 s
-  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
-  , (Event 7,Event 3),  (NoEvent,NoEvent)  -- 3.5 s
-  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
-  , (Event 8,Event 4),  (NoEvent,NoEvent)  -- 4.0 s
-  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
-  , (Event 9,Event 5),  (NoEvent,NoEvent)  -- 4.5 s
-  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
-  , (Event 10,Event 6), (NoEvent,NoEvent)  -- 5.0 s
-  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
-  , (Event 11,Event 7), (NoEvent,NoEvent)  -- 5.5 s
-  , (NoEvent,NoEvent),  (NoEvent,NoEvent)
-  , (Event 12,Event 8), (NoEvent,NoEvent)  -- 6.0 s
-  ]
-
-evsrc_t29 :: [Event [Double]]
-evsrc_t29 = embed (time &&& repeatedly 0.5001 ()
-                   >>> arr (\(t,e) -> e `tag` t)
-                   >>> delayEventCat 3.0)
-                  input
-  where
-    dts   = replicate 40 0.1 ++ [2.0] ++ replicate 40 0.1
-    input = ((), [(dt, Just ()) | dt <- dts])
-
-{- Resulting input to the delay for reference:
-[ NoEvent,   NoEvent,   NoEvent, NoEvent, NoEvent  -- 0.0 s
-, NoEvent,   Event 0.6, NoEvent, NoEvent, NoEvent  -- 0.5 s
-, NoEvent,   Event 1.1, NoEvent, NoEvent, NoEvent  -- 1.0 s
-, NoEvent,   Event 1.6, NoEvent, NoEvent, NoEvent  -- 1.5 s
-, NoEvent,   Event 2.1, NoEvent, NoEvent, NoEvent  -- 2.0 s
-, NoEvent,   Event 2.6, NoEvent, NoEvent, NoEvent  -- 2.5 s
-, NoEvent,   Event 3.1, NoEvent, NoEvent, NoEvent  -- 3.0 s
-, NoEvent,   Event 3.6, NoEvent, NoEvent, NoEvent  -- 3.5 s
-, NoEvent                                          -- 4.0 s
-, Event 6.0, Event 6.1, NoEvent, NoEvent, NoEvent  -- 6.0 s
-, NoEvent,   Event 6.6, NoEvent, NoEvent, NoEvent  -- 6.5 s
-, NoEvent,   Event 7.1, NoEvent, NoEvent, NoEvent  -- 7.0 s
-, NoEvent,   Event 7.6, NoEvent, NoEvent, NoEvent  -- 7.5 s
-, NoEvent,   Event 8.1, NoEvent, NoEvent, NoEvent  -- 8.0 s
-, NoEvent,   Event 8.6, NoEvent, NoEvent, NoEvent  -- 8.5 s
-, NoEvent,   Event 9.1, NoEvent, NoEvent, NoEvent  -- 9.0 s
-, NoEvent,   Event 9.6, NoEvent, NoEvent, NoEvent  -- 9.5 s
-, NoEvent                                          -- 10.0 s
-]
--}
-
-evsrc_t29r =
-  [ NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 0.0 s
-  , NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 0.5 s
-  , NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 1.0 s
-  , NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 1.5 s
-  , NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 2.0 s
-  , NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 2.5 s
-  , NoEvent, NoEvent,     NoEvent, NoEvent, NoEvent  -- 3.0 s
-  , NoEvent, Event [0.6], NoEvent, NoEvent, NoEvent  -- 3.5 s
-  , NoEvent                                          -- 4.0 s
-  , Event [1.1, 1.6, 2.1, 2.6]                       -- 6.0 s
-           , NoEvent,     Event [3.1], NoEvent, NoEvent
-  , NoEvent, NoEvent,     Event [3.6], NoEvent, NoEvent  -- 6.5 s
-  , NoEvent, NoEvent,     NoEvent,     NoEvent, NoEvent  -- 7.0 s
-  , NoEvent, NoEvent,     NoEvent,     NoEvent, NoEvent  -- 7.5 s
-  , NoEvent, NoEvent,     NoEvent,     NoEvent, NoEvent  -- 8.0 s
-  , NoEvent, NoEvent,     NoEvent,     NoEvent, NoEvent  -- 8.5 s
-  , NoEvent, Event [6.0], Event [6.1], NoEvent, NoEvent  -- 9.0 s
-  , NoEvent, NoEvent,     Event [6.6], NoEvent, NoEvent  -- 9.5 s
-  , NoEvent                                              -- 10.0 s
-  ]
-
--- "delayEvent" in a feedback loop. Should work like "repeatedly".
-evsrc_t30 :: [(Event ())]
-evsrc_t30 = embed (now ()
-                   >>> (loop $
-                          arr (uncurry lMerge)
-                          >>> delayEvent 1.0
-                          >>> arr dup))
-                  (deltaEncode 0.125 (replicate 50 ()))
-
-evsrc_t30r :: [(Event ())]
-evsrc_t30r =
-  [ NoEvent,  NoEvent, NoEvent, NoEvent  -- 0.0 s
-  , NoEvent,  NoEvent, NoEvent, NoEvent  -- 0.5 s
-  , Event (), NoEvent, NoEvent, NoEvent  -- 1.0 s
-  , NoEvent,  NoEvent, NoEvent, NoEvent  -- 1.5 s
-  , Event (), NoEvent, NoEvent, NoEvent  -- 2.0 s
-  , NoEvent,  NoEvent, NoEvent, NoEvent  -- 2.5 s
-  , Event (), NoEvent, NoEvent, NoEvent  -- 3.0 s
-  , NoEvent,  NoEvent, NoEvent, NoEvent  -- 3.5 s
-  , Event (), NoEvent, NoEvent, NoEvent  -- 4.0 s
-  , NoEvent,  NoEvent, NoEvent, NoEvent  -- 4.5 s
-  , Event (), NoEvent, NoEvent, NoEvent  -- 5.0 s
-  , NoEvent,  NoEvent, NoEvent, NoEvent  -- 5.5 s
-  , Event (), NoEvent                    -- 6.0 s
-  ]
-
--- * Test cases for utilities (Utils)
+-- ** Hybrid continuous-to-discrete SF combinators.
 
 utils_t10 :: [Event Double]
 utils_t10 = testSF1 snap
@@ -648,32 +686,6 @@ utils_t12r =
   , Event 16.0, NoEvent, NoEvent, NoEvent  -- 4.0 s
   , Event 20.0, NoEvent, NoEvent, NoEvent  -- 5.0 s
   , Event 24.0
-  ]
-
-utils_t13 :: [Event ()]
-utils_t13 = testSF1 (recur (after 0.99 ()))
-
-utils_t13r =
-  [ NoEvent,  NoEvent, NoEvent, NoEvent  -- 0.0 s
-  , Event (), NoEvent, NoEvent, NoEvent  -- 1.0 s
-  , Event (), NoEvent, NoEvent, NoEvent  -- 2.0 s
-  , Event (), NoEvent, NoEvent, NoEvent  -- 3.0 s
-  , Event (), NoEvent, NoEvent, NoEvent  -- 4.0 s
-  , Event (), NoEvent, NoEvent, NoEvent  -- 5.0 s
-  , Event ()
-  ]
-
-utils_t14 :: [Event Int]
-utils_t14 = testSF1 (after 1.0 1 `andThen` now 2 `andThen` after 2.0 3)
-
-utils_t14r =
-  [ NoEvent, NoEvent, NoEvent, NoEvent  -- 0.0 s
-  , Event 1, NoEvent, NoEvent, NoEvent  -- 1.0 s
-  , NoEvent, NoEvent, NoEvent, NoEvent  -- 2.0 s
-  , Event 3, NoEvent, NoEvent, NoEvent  -- 3.0 s
-  , NoEvent, NoEvent, NoEvent, NoEvent  -- 4.0 s
-  , NoEvent, NoEvent, NoEvent, NoEvent  -- 5.0 s
-  , NoEvent
   ]
 
 utils_t15 = take 50 (embed (time >>> sampleWindow 5 0.5)
@@ -753,42 +765,35 @@ utils_t16r =
   , Event [7.0, 7.0, 7.0, 7.5, 8.0]                             -- 8.0
   ]
 
--- Events
-prop_event_noevent =
-    forAll myStream $ evalT $ Always $ prop (sfNever, const (== noEvent))
+-- * Repetition and switching
 
-  where myStream :: Gen (SignalSampleStream Float)
-        myStream = uniDistStream
-        sfNever :: SF Float (Event Float)
-        sfNever = never
+utils_t13 :: [Event ()]
+utils_t13 = testSF1 (recur (after 0.99 ()))
 
-prop_event_now =
-    forAll myStream $ evalT $
-      -- (sf, p0) /\ O [] (sf, pn)
-      And (prop (sf, p0))                 -- Initially
-          (Next $ Always $ prop (sf, pn)) -- After first sample
+utils_t13r =
+  [ NoEvent,  NoEvent, NoEvent, NoEvent  -- 0.0 s
+  , Event (), NoEvent, NoEvent, NoEvent  -- 1.0 s
+  , Event (), NoEvent, NoEvent, NoEvent  -- 2.0 s
+  , Event (), NoEvent, NoEvent, NoEvent  -- 3.0 s
+  , Event (), NoEvent, NoEvent, NoEvent  -- 4.0 s
+  , Event (), NoEvent, NoEvent, NoEvent  -- 5.0 s
+  , Event ()
+  ]
 
-  where sf = Yampa.now 42.0
+utils_t14 :: [Event Int]
+utils_t14 = testSF1 (after 1.0 1 `andThen` now 2 `andThen` after 2.0 3)
 
-        p0 x y = y == Event 42.0
-        pn x y = y == noEvent
+utils_t14r =
+  [ NoEvent, NoEvent, NoEvent, NoEvent  -- 0.0 s
+  , Event 1, NoEvent, NoEvent, NoEvent  -- 1.0 s
+  , NoEvent, NoEvent, NoEvent, NoEvent  -- 2.0 s
+  , Event 3, NoEvent, NoEvent, NoEvent  -- 3.0 s
+  , NoEvent, NoEvent, NoEvent, NoEvent  -- 4.0 s
+  , NoEvent, NoEvent, NoEvent, NoEvent  -- 5.0 s
+  , NoEvent
+  ]
 
-        myStream :: Gen (SignalSampleStream Float)
-        myStream = uniDistStream
-
-prop_event_after_0 =
-    forAll myStream $ evalT $
-      -- (sf, p0) /\ O [] (sf, pn)
-      And (prop (sf, p0))                 -- Initially
-          (Next $ Always $ prop (sf, pn)) -- After first sample
-
-  where sf = after 0.0 42.0
-
-        p0 x y = y == Event 42.0
-        pn x y = y == noEvent
-
-        myStream :: Gen (SignalSampleStream Float)
-        myStream = uniDistStream
+-- * Auxiliary
 
 -- prop :: SF a b -> (a -> b ->
 prop (a,b) = SP ((identity &&& a) >>^ uncurry b)
