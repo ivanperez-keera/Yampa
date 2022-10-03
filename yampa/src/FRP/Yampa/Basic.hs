d29da8075a65bad4ed3ba14d9fdbc3cafc9adb90
Ivan Perez <ivan.perez@keera.co.uk>
2022-10-03 12:46:55 +0000

    yampa: Adjust years, copyright owners. Refs #235.
    Yampa is BSD3 with a copyright notice that points to a single year, but
    development has been led by multiple people over the years, who have
    made numerous contributions.
    
    This commit adjusts the license and the copyright holders listed in the
    haddock comments to match the main developers over those years. The
    years and owners have been agreed with the original creators, Antony
    Courtney and Henrik Nilsson. Feedback from George Giorgidge, a main
    developer and contributor for 5 years, has also been sought.

--- yampa/src/FRP/Yampa/Basic.hs
+++ yampa/src/FRP/Yampa/Basic.hs
@@ -1,6 +1,9 @@
 -- |
 -- Module      :  FRP.Yampa.Basic
--- Copyright   :  (c) Antony Courtney and Henrik Nilsson, Yale University, 2003
+-- Copyright   :  (c) Ivan Perez, 2014-2022
+--                (c) George Giorgidze, 2007-2012
+--                (c) Henrik Nilsson, 2005-2006
+--                (c) Antony Courtney and Henrik Nilsson, Yale University, 2003-2004
 -- License     :  BSD-style (see the LICENSE file in the distribution)
 --
 -- Maintainer  :  ivan.perez@keera.co.uk
