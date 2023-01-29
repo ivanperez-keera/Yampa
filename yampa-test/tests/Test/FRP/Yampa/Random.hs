0026385f2243ebf03f267b2e89d1898b7ebb3cf1
Ivan Perez <ivan.perez@keera.co.uk>
2023-01-29 12:29:43 +0000

    yampa-test: Add module to test SFs producing random values. Refs #248.
    This commit adds an empty module to test the module FRP.Yampa.Random.
    The module is enabled in the testing suite, but, as of this commit,
    contains no properties.

---
+++ yampa-test/tests/Test/FRP/Yampa/Random.hs
@@ -0,0 +1,14 @@
+-- |
+-- Description : Test cases for signal functions working with random values.
+-- Copyright   : (c) Ivan Perez, 2023
+-- Authors     : Ivan Perez
+
+module Test.FRP.Yampa.Random
+    ( tests
+    )
+  where
+
+import Test.Tasty (TestTree, testGroup)
+
+tests :: TestTree
+tests = testGroup "Regression tests for FRP.Yampa.Random" []
