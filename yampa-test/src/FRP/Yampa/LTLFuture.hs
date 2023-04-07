697339002bf16f8fda293509991133a2348bdeb4
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-07 03:21:58 +0000

    yampa-test: Wrap lines to 80 characters (KSG Haskell 1.3.0 - 4.3). Refs #256.
    We set the code limit for Haskell files at 80 characters.

--- yampa-test/src/FRP/Yampa/LTLFuture.hs
+++ yampa-test/src/FRP/Yampa/LTLFuture.hs
@@ -9,14 +9,13 @@
 -- This module contains a definition of LTL with Next on top of Signal
 -- Functions.
 --
--- LTL predicates are parameterized over an input. A basic proposition
--- is a Signal Function that produces a boolean function.
+-- LTL predicates are parameterized over an input. A basic proposition is a
+-- Signal Function that produces a boolean function.
 
--- Important question: because this FRP implement uses CPS,
--- it is stateful, and sampling twice in one time period
--- is not necessarily the same as sampling once. This means that
--- tauApp, or next, might not work correctly. It's important to
--- see what is going on there... :(
+-- Important question: because this FRP implement uses CPS, it is stateful, and
+-- sampling twice in one time period is not necessarily the same as sampling
+-- once. This means that tauApp, or next, might not work correctly. It's
+-- important to see what is going on there... :(
 
 module FRP.Yampa.LTLFuture
     ( TPred(..)
@@ -52,22 +51,31 @@ evalT (And t1 t2)     = \stream -> evalT t1 stream && evalT t2 stream
 evalT (Or  t1 t2)     = \stream -> evalT t1 stream || evalT t2 stream
 evalT (Not t1)        = \stream -> not (evalT t1 stream)
 evalT (Implies t1 t2) = \stream -> not (evalT t1 stream) || evalT t2 stream
-evalT (Always  t1)    = \stream -> evalT t1 stream && evalT (Next (Always t1)) stream
-evalT (Eventually t1) = \stream -> case stream of
-                                     (a,[])          -> evalT t1 stream
-                                     (a1,(dt,a2):as) -> evalT t1 stream || evalT (tauApp (Eventually t1) a1 dt) (a2, as)
-evalT (Until t1 t2)   = \stream -> (evalT t1 stream && evalT (Next (Until t1 t2)) stream)
-                                   || evalT t2 stream
-evalT (Next t1)       = \stream -> case stream of
-                                     (a,[]) -> True   -- This is important. It determines how
-                                                      -- always and next behave at the
-                                                      -- end of the stream, which affects that is and isn't
-                                                      -- a tautology. It should be reviewed very carefully.
-                                     (a1,(dt, a2):as) -> evalT (tauApp t1 a1 dt) (a2, as)
+evalT (Always  t1)    = \stream ->
+  evalT t1 stream && evalT (Next (Always t1)) stream
+
+evalT (Eventually t1) = \stream ->
+  case stream of
+    (a,[])          -> evalT t1 stream
+    (a1,(dt,a2):as) -> evalT t1 stream
+                         || evalT (tauApp (Eventually t1) a1 dt) (a2, as)
+
+evalT (Until t1 t2)   = \stream ->
+  (evalT t1 stream && evalT (Next (Until t1 t2)) stream)
+    || evalT t2 stream
+
+evalT (Next t1)       = \stream ->
+  case stream of
+    (a,[]) -> True   -- This is important. It determines how
+                     -- always and next behave at the
+                     -- end of the stream, which affects that is and isn't
+                     -- a tautology. It should be reviewed very carefully.
+    (a1,(dt, a2):as) -> evalT (tauApp t1 a1 dt) (a2, as)
 
 -- | Tau-application (transportation to the future)
 tauApp :: TPred a -> a -> DTime -> TPred a
-tauApp pred sample dtime = tPredMap (\sf -> snd (evalFuture sf sample dtime)) pred
+tauApp pred sample dtime =
+  tPredMap (\sf -> snd (evalFuture sf sample dtime)) pred
 
 -- | Apply a transformation to the leaves (to the SFs)
 tPredMap :: (SF a Bool -> SF a Bool) -> TPred a -> TPred a
