a444190304ca2544a8a4602a56e587a8199c9b5c
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-29 17:53:54 +0000

    yampa: Remove unnecessary horizontal space (KSG Haskell 1.3.0 - 4.5.4.3). Refs #266.
    Do not leave any extra spaces except as mandated or recommended by these
    rules.

--- yampa/src/FRP/Yampa/Arrow.hs
+++ yampa/src/FRP/Yampa/Arrow.hs
@@ -1,14 +1,14 @@
 -- |
--- Module      :  FRP.Yampa.Arrow
--- Copyright   :  (c) Ivan Perez, 2014-2022
---                (c) George Giorgidze, 2007-2012
---                (c) Henrik Nilsson, 2005-2006
---                (c) Antony Courtney and Henrik Nilsson, Yale University, 2003-2004
--- License     :  BSD-style (see the LICENSE file in the distribution)
+-- Module      : FRP.Yampa.Arrow
+-- Copyright   : (c) Ivan Perez, 2014-2022
+--               (c) George Giorgidze, 2007-2012
+--               (c) Henrik Nilsson, 2005-2006
+--               (c) Antony Courtney and Henrik Nilsson, Yale University, 2003-2004
+-- License     : BSD-style (see the LICENSE file in the distribution)
 --
--- Maintainer  :  ivan.perez@keera.co.uk
--- Stability   :  provisional
--- Portability :  portable
+-- Maintainer  : ivan.perez@keera.co.uk
+-- Stability   : provisional
+-- Portability : portable
 --
 -- Arrow helper functions.
 module FRP.Yampa.Arrow
