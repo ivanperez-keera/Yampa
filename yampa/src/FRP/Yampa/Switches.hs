e5737be3ef1a50f03d8f1a3c1f128dd34f6377bb
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-29 16:00:42 +0000

    yampa: Adjust indentation to 2 spaces (KSG Haskell 1.3.0 - 4.2). Refs #266.
    Each time a new expression or block-like construct is opened, the indent
    should increase by two spaces. When the section ends, the indent returns
    to the previous indent level. The indent level applies to both Haskell
    expressions and comments throughout the block.
    
    This commit removes unncessary indentation.

--- yampa/src/FRP/Yampa/Switches.hs
+++ yampa/src/FRP/Yampa/Switches.hs
@@ -432,11 +432,11 @@ drpSwitchB = drpSwitch broadcast
 --
 par :: Functor col
     => (forall sf . (a -> col sf -> col (b, sf)))
-         -- ^ Determines the input to each signal function
-         --     in the collection. IMPORTANT! The routing function MUST
-         --     preserve the structure of the signal function collection.
+       -- ^ Determines the input to each signal function in the collection.
+       -- IMPORTANT! The routing function MUST preserve the structure of the
+       -- signal function collection.
     -> col (SF b c)
-         -- ^ Signal function collection.
+       -- ^ Signal function collection.
     -> SF a (col c)
 par rf sfs0 = SF {sfTF = tf0}
   where
@@ -470,16 +470,16 @@ parAux rf sfs = SF' tf -- True
 -- event value.
 pSwitch :: Functor col
         => (forall sf . (a -> col sf -> col (b, sf)))
-              -- ^ Routing function: determines the input to each signal
-              -- function in the collection. IMPORTANT! The routing function
-              -- has an obligation to preserve the structure of the signal
-              -- function collection.
+           -- ^ Routing function: determines the input to each signal function
+           -- in the collection. IMPORTANT! The routing function has an
+           -- obligation to preserve the structure of the signal function
+           -- collection.
         -> col (SF b c)
-              -- ^ Signal function collection.
+           -- ^ Signal function collection.
         -> SF (a, col c) (Event d)
-              -- ^ Signal function generating the switching event.
+           -- ^ Signal function generating the switching event.
         -> (col (SF b c) -> d -> SF a (col c))
-              -- ^ Continuation to be invoked once event occurs.
+           -- ^ Continuation to be invoked once event occurs.
         -> SF a (col c)
 pSwitch rf sfs0 sfe0 k = SF {sfTF = tf0}
   where
@@ -517,24 +517,23 @@ pSwitch rf sfs0 sfe0 k = SF {sfTF = tf0}
 -- other signal functions.
 dpSwitch :: Functor col
          => (forall sf . (a -> col sf -> col (b, sf)))
-              -- ^ Routing function. Its purpose is to pair up each running
-              -- signal function in the collection maintained by 'dpSwitch'
-              -- with the input it is going to see at each point in time. All
-              -- the routing function can do is specify how the input is
-              -- distributed.
+            -- ^ Routing function. Its purpose is to pair up each running
+            -- signal function in the collection maintained by 'dpSwitch' with
+            -- the input it is going to see at each point in time. All the
+            -- routing function can do is specify how the input is distributed.
          -> col (SF b c)
-              -- ^ Initial collection of signal functions.
+            -- ^ Initial collection of signal functions.
          -> SF (a, col c) (Event d)
-              -- ^ Signal function that observes the external input signal and
-              -- the output signals from the collection in order to produce a
-              -- switching event.
+            -- ^ Signal function that observes the external input signal and
+            -- the output signals from the collection in order to produce a
+            -- switching event.
          -> (col (SF b c) -> d -> SF a (col c))
-              -- ^ The fourth argument is a function that is invoked when the
-              -- switching event occurs, yielding a new signal function to
-              -- switch into based on the collection of signal functions
-              -- previously running and the value carried by the switching
-              -- event. This allows the collection to be updated and then
-              -- switched back in, typically by employing 'dpSwitch' again.
+            -- ^ The fourth argument is a function that is invoked when the
+            -- switching event occurs, yielding a new signal function to switch
+            -- into based on the collection of signal functions previously
+            -- running and the value carried by the switching event. This
+            -- allows the collection to be updated and then switched back in,
+            -- typically by employing 'dpSwitch' again.
          -> SF a (col c)
 dpSwitch rf sfs0 sfe0 k = SF {sfTF = tf0}
   where
@@ -574,12 +573,12 @@ dpSwitch rf sfs0 sfe0 k = SF {sfTF = tf0}
 -- This is the parallel version of 'rSwitch'.
 rpSwitch :: Functor col
          => (forall sf . (a -> col sf -> col (b, sf)))
-               -- ^ Routing function: determines the input to each signal
-               -- function in the collection. IMPORTANT! The routing function
-               -- has an obligation to preserve the structure of the signal
-               -- function collection.
+            -- ^ Routing function: determines the input to each signal function
+            -- in the collection. IMPORTANT! The routing function has an
+            -- obligation to preserve the structure of the signal function
+            -- collection.
          -> col (SF b c)
-               -- ^ Initial signal function collection.
+            -- ^ Initial signal function collection.
          -> SF (a, Event (col (SF b c) -> col (SF b c))) (col c)
 rpSwitch rf sfs =
   pSwitch (rf . fst) sfs (arr (snd . fst)) $ \sfs' f ->
@@ -599,12 +598,12 @@ rpSwitch rf sfs =
 -- This is the parallel version of 'drSwitch'.
 drpSwitch :: Functor col
           => (forall sf . (a -> col sf -> col (b, sf)))
-                -- ^ Routing function: determines the input to each signal
-                -- function in the collection. IMPORTANT! The routing function
-                -- has an obligation to preserve the structure of the signal
-                -- function collection.
+             -- ^ Routing function: determines the input to each signal
+             -- function in the collection. IMPORTANT! The routing function has
+             -- an obligation to preserve the structure of the signal function
+             -- collection.
           -> col (SF b c)
-                -- ^ Initial signal function collection.
+             -- ^ Initial signal function collection.
           -> SF (a, Event (col (SF b c) -> col (SF b c))) (col c)
 drpSwitch rf sfs =
   dpSwitch (rf . fst) sfs (arr (snd . fst)) $ \sfs' f ->
