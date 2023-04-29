2a0259ab525ac224c849bc13ba13fb257b957e0a
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-29 17:56:22 +0000

    yampa: Remove unnecessary horizontal space (KSG Haskell 1.3.0 - 4.5.4.3). Refs #266.
    Do not leave any extra spaces except as mandated or recommended by these
    rules.

--- yampa/src/FRP/Yampa/Simulation.hs
+++ yampa/src/FRP/Yampa/Simulation.hs
@@ -1,14 +1,14 @@
 -- |
--- Module      :  FRP.Yampa.Simulation
--- Copyright   :  (c) Ivan Perez, 2014-2022
---                (c) George Giorgidze, 2007-2012
---                (c) Henrik Nilsson, 2005-2006
---                (c) Antony Courtney and Henrik Nilsson, Yale University, 2003-2004
--- License     :  BSD-style (see the LICENSE file in the distribution)
+-- Module      : FRP.Yampa.Simulation
+-- Copyright   : (c) Ivan Perez, 2014-2022
+--               (c) George Giorgidze, 2007-2012
+--               (c) Henrik Nilsson, 2005-2006
+--               (c) Antony Courtney and Henrik Nilsson, Yale University, 2003-2004
+-- License     : BSD-style (see the LICENSE file in the distribution)
 --
--- Maintainer  :  ivan.perez@keera.co.uk
--- Stability   :  provisional
--- Portability :  non-portable (GHC extensions)
+-- Maintainer  : ivan.perez@keera.co.uk
+-- Stability   : provisional
+-- Portability : non-portable (GHC extensions)
 --
 -- Execution/simulation of signal functions.
 --
