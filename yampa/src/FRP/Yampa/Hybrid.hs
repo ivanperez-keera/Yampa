d25302a5cfa01e509d5fe0da121ce866e7dfe948
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-29 15:46:17 +0000

    yampa: Replace underscores in variable names (KSG Haskell 1.3.0 - 4.1). Refs #266.
    Underscore should not be used in names, except to indicate lack of
    effects (as the last character in the name) or in functions that export
    elements from other libraries via the FFI.

--- yampa/src/FRP/Yampa/Hybrid.hs
+++ yampa/src/FRP/Yampa/Hybrid.hs
@@ -50,7 +50,7 @@ import FRP.Yampa.InternalCore (SF, epPrim)
 -- >>> embed (hold 1) (deltaEncode 0.1 [NoEvent, NoEvent, Event 2, NoEvent, Event 3, NoEvent])
 -- [1,1,2,2,3,3]
 hold :: a -> SF (Event a) a
-hold a_init = epPrim f () a_init
+hold aInit = epPrim f () aInit
   where
     f _ a = ((), a, a)
 
@@ -75,7 +75,7 @@ dHold a0 = hold a0 >>> iPre a0
 -- >>> embed (trackAndHold 1) (deltaEncode 0.1 [Nothing, Nothing, Just 2, Nothing, Just 3, Nothing])
 -- [1,1,2,2,3,3]
 trackAndHold :: a -> SF (Maybe a) a
-trackAndHold a_init = arr (maybe NoEvent Event) >>> hold a_init
+trackAndHold aInit = arr (maybe NoEvent Event) >>> hold aInit
 
 -- | Tracks input signal when available, holding the last value when the input
 -- is 'Nothing', with a delay.
@@ -87,7 +87,7 @@ trackAndHold a_init = arr (maybe NoEvent Event) >>> hold a_init
 -- [1,1,1,2,2,3]
 
 dTrackAndHold :: a -> SF (Maybe a) a
-dTrackAndHold a_init = trackAndHold a_init >>> iPre a_init
+dTrackAndHold aInit = trackAndHold aInit >>> iPre aInit
 
 -- * Accumulators
 
@@ -99,7 +99,7 @@ dTrackAndHold a_init = trackAndHold a_init >>> iPre a_init
 --   whose new value is outputted in an 'Event'.
 --
 accum :: a -> SF (Event (a -> a)) (Event a)
-accum a_init = epPrim f a_init NoEvent
+accum aInit = epPrim f aInit NoEvent
   where
     f a g = (a', Event a', NoEvent) -- Accumulator, output if Event,
                                     -- output if no event
@@ -109,7 +109,7 @@ accum a_init = epPrim f a_init NoEvent
 -- | Zero-order hold accumulator (always produces the last outputted value
 --   until an event arrives).
 accumHold :: a -> SF (Event (a -> a)) a
-accumHold a_init = epPrim f a_init a_init
+accumHold aInit = epPrim f aInit aInit
   where
     f a g = (a', a', a') -- Accumulator, output if Event, output if no event
       where
@@ -119,11 +119,11 @@ accumHold a_init = epPrim f a_init a_init
 -- the last outputted value until an event arrives, but the very initial output
 -- is always the given accumulator).
 dAccumHold :: a -> SF (Event (a -> a)) a
-dAccumHold a_init = accumHold a_init >>> iPre a_init
+dAccumHold aInit = accumHold aInit >>> iPre aInit
 
 -- | Accumulator parameterized by the accumulation function.
 accumBy :: (b -> a -> b) -> b -> SF (Event a) (Event b)
-accumBy g b_init = epPrim f b_init NoEvent
+accumBy g bInit = epPrim f bInit NoEvent
   where
     f b a = (b', Event b', NoEvent)
       where
@@ -131,7 +131,7 @@ accumBy g b_init = epPrim f b_init NoEvent
 
 -- | Zero-order hold accumulator parameterized by the accumulation function.
 accumHoldBy :: (b -> a -> b) -> b -> SF (Event a) b
-accumHoldBy g b_init = epPrim f b_init b_init
+accumHoldBy g bInit = epPrim f bInit bInit
   where
     f b a = (b', b', b')
       where
@@ -141,14 +141,14 @@ accumHoldBy g b_init = epPrim f b_init b_init
 --   with delayed initialization (initial output sample is always the
 --   given accumulator).
 dAccumHoldBy :: (b -> a -> b) -> b -> SF (Event a) b
-dAccumHoldBy f a_init = accumHoldBy f a_init >>> iPre a_init
+dAccumHoldBy f aInit = accumHoldBy f aInit >>> iPre aInit
 
 -- | Accumulator parameterized by the accumulator function with filtering,
 --   possibly discarding some of the input events based on whether the second
 --   component of the result of applying the accumulation function is
 --   'Nothing' or 'Just' x for some x.
 accumFilter :: (c -> a -> (c, Maybe b)) -> c -> SF (Event a) (Event b)
-accumFilter g c_init = epPrim f c_init NoEvent
+accumFilter g cInit = epPrim f cInit NoEvent
   where
     f c a = case g c a of
               (c', Nothing) -> (c', NoEvent, NoEvent)
