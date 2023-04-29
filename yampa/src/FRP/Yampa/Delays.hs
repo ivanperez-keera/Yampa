c783f408a0fbe38353da1d15ad148b59249c4253
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-29 15:38:15 +0000

    yampa: Replace underscores in variable names (KSG Haskell 1.3.0 - 4.1). Refs #266.
    Underscore should not be used in names, except to indicate lack of
    effects (as the last character in the name) or in functions that export
    elements from other libraries via the FFI.

--- yampa/src/FRP/Yampa/Delays.hs
+++ yampa/src/FRP/Yampa/Delays.hs
@@ -71,34 +71,34 @@ b0 `fby` sf = b0 --> sf >>> pre
 -- | Delay a signal by a fixed time 't', using the second parameter
 -- to fill in the initial 't' seconds.
 delay :: Time -> a -> SF a a
-delay q a_init | q < 0     = usrErr "Yampa" "delay" "Negative delay."
-               | q == 0    = identity
-               | otherwise = SF {sfTF = tf0}
+delay q aInit | q < 0     = usrErr "Yampa" "delay" "Negative delay."
+              | q == 0    = identity
+              | otherwise = SF {sfTF = tf0}
   where
-    tf0 a0 = (delayAux [] [(q, a0)] 0 a_init, a_init)
+    tf0 a0 = (delayAux [] [(q, a0)] 0 aInit, aInit)
 
     -- Invariants:
-    -- t_diff measure the time since the latest output sample ideally
+    -- tDiff measure the time since the latest output sample ideally
     -- should have been output. Whenever that equals or exceeds the
     -- time delta for the next buffered sample, it is time to output a
     -- new sample (although not necessarily the one first in the queue:
     -- it might be necessary to "catch up" by discarding samples.
-    -- 0 <= t_diff < bdt, where bdt is the buffered time delta for the
+    -- 0 <= tDiff < bdt, where bdt is the buffered time delta for the
     -- sample on the front of the buffer queue.
     --
     -- Sum of time deltas in the queue >= q.
     delayAux _ [] _ _ = undefined
-    delayAux rbuf buf@((bdt, ba) : buf') t_diff a_prev = SF' tf -- True
+    delayAux rbuf buf@((bdt, ba) : buf') tDiff aPrev = SF' tf -- True
       where
-        tf dt a | t_diff' < bdt =
-                    (delayAux rbuf' buf t_diff' a_prev, a_prev)
-                | otherwise = nextSmpl rbuf' buf' (t_diff' - bdt) ba
+        tf dt a | tDiff' < bdt =
+                    (delayAux rbuf' buf tDiff' aPrev, aPrev)
+                | otherwise = nextSmpl rbuf' buf' (tDiff' - bdt) ba
           where
-            t_diff' = t_diff + dt
-            rbuf'   = (dt, a) : rbuf
+            tDiff' = tDiff + dt
+            rbuf'  = (dt, a) : rbuf
 
-            nextSmpl rbuf [] t_diff a =
-              nextSmpl [] (reverse rbuf) t_diff a
-            nextSmpl rbuf buf@((bdt, ba) : buf') t_diff a
-              | t_diff < bdt = (delayAux rbuf buf t_diff a, a)
-              | otherwise    = nextSmpl rbuf buf' (t_diff-bdt) ba
+            nextSmpl rbuf [] tDiff a =
+              nextSmpl [] (reverse rbuf) tDiff a
+            nextSmpl rbuf buf@((bdt, ba) : buf') tDiff a
+              | tDiff < bdt = (delayAux rbuf buf tDiff a, a)
+              | otherwise   = nextSmpl rbuf buf' (tDiff-bdt) ba
